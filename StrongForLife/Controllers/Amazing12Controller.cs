01edb274470434e876b9b7e698431907a7120a53
mark.heaver <mark.heaver@tombola.com>
2014-10-26 17:43:13 +0000

    adding amazing12 initial content

---
+++ StrongForLife/Controllers/Amazing12Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StrongForLife.Controllers
+{
+    public class Amazing12Controller : Controller
+    {
+        //
+        // GET: /Amazing12/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
