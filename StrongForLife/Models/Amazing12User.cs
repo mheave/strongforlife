eca6c6875b0733116825d020821c0c4c1d0b8848
mark.heaver <mark.heaver@tombola.com>
2014-11-10 21:23:14 +0000

    confirmation page added

---
+++ StrongForLife/Models/Amazing12User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StrongForLife.Models {
+	public class Amazing12User {
+		public string FirstName { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public int MobileNumber { get; set; }
+		public string TakePartReason { get; set; }
+		public string Values { get; set; }
+		public string Motivation { get; set; }
+		public DietSuccess DietSuccess { get; set; }
+		public TrainingPreference TrainingPreference { get; set; }
+
+	}
+
+	public enum DietSuccess
+	{
+		Yes,
+		No,
+		Sometimes
+	}
+
+	public enum TrainingPreference {
+		Morning,
+		Daytime,
+		Evenings
+	}
+}
